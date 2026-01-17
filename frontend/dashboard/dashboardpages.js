2de9cb2d616d6e22af000b61c2edd1526074a2d7
THEJAS-BK <thejasbk998@gmail.com>
2026-01-17 18:04:14 +0000

    updates

--- frontend/dashboard/dashboardpages.js
+++ frontend/dashboard/dashboardpages.js
@@ -3,3 +3,14 @@ const createGroupBtn = document.querySelector(".create-group-btn")
 createGroupBtn.addEventListener("click",()=>{
   window.location.href="/frontend/dashboard/user/creategroup.html"
 })
+//Load userpage Groups
+document.addEventListener("DOMContentLoaded",()=>{
+  const groupContainer = document.querySelector(".leftTab")
+  //individual group
+  const div = document.createElement("div");
+  groupContainer.append(div)
+  div.classList.add("leftTab-Tab")
+  
+  groupContainer.append(div)
+
+})
