f6238788fdcef304d44b556e92bc32986596faf0
THEJAS-BK <thejasbk998@gmail.com>
2026-02-07 07:37:50 +0000

    updates

--- frontend/dashboard/user/groupinterface.js
+++ frontend/dashboard/user/groupinterface.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const params = new URLSearchParams(window.location.search);
-   console.log(params.get("id"))
+  const params = new URLSearchParams(window.location.search);
+ const id = params.get("id");
+
+  //add issues
+  const addIssueBtn = document.querySelector(".addIssue-btn");
+  addIssueBtn.addEventListener("click", () => {
+        window.location.href=`/frontend/dashboard/user/addIssue.html?id=${id}`
+  });
 });
