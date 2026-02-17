f191fb0059533c25143e8f8d0d2b575cbe17bd0f
THEJAS-BK <thejasbk998@gmail.com>
2026-02-17 16:02:31 +0000

    all routes organized

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,6 @@ const authRoutes = require("./routes/auth.routes");
 const groupRoutes=require("./routes/group.routes");
 const issueRoutes = require("./routes/issue.routes")
 
-//Middlewares
-const { authorizationToken } = require("./middlewares/auth.middleware");
-
 //setups
 const app = express();
 app.use(express.json());
@@ -33,11 +30,9 @@ main()
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
-//Schemas
-const Group = require("./models/group");
+//utils
 const ExpressError = require("./utils/ExpressError");
-const Issue = require("./models/issue");
-//uitls
+
 
 
 //! Routes
@@ -50,45 +45,6 @@ app.use("/auth", authRoutes);
 app.use("/groups",groupRoutes);
 //?issues
 app.use("/issues",issueRoutes);
-//!!! admin routes
-//!search join request
-app.get(
-  "/join/request/:groupId/admin/search",
-  authorizationToken,
-  async (req, res, next) => {
-    try {
-      const { groupId } = req.params;
-      const val = req.query.q;
-      if (!groupId || !val) return res.sendStatus(400);
-
-      const curUser = req.user.userId;
-      if (!curUser) return res.sendStatus(401);
-
-      const allmembers = await Group.findById(groupId).select("members");
-
-      const curUserRole = allmembers.members.find((mem) => {
-        return mem.userId.toString() === curUser;
-      }).role;
-
-      if (curUserRole !== "admin" && curUserRole !== "coadmin")
-        return res.status(403).json({ message: "unauthorized" });
-
-      const requests = await Group.findById(groupId)
-        .select("joinRequests")
-        .populate("joinRequests.userId", "name");
-
-      const regex = new RegExp(val, "i");
-      const members = requests.joinRequests.filter((mem) => {
-        return regex.test(mem.userId.name);
-      });
-
-      res.json(members);
-    } catch (err) {
-      next(err);
-    }
-  },
-);
-
 //404 route
 app.all("/*splat", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
